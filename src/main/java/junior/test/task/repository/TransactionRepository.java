4771fde2c3c7d7ca3e10171844e865e45f8eb6f2
Akkl <Aklbktglv@gmail.com>
2023-09-19 22:38:54 +0600

    limit exceeded List

--- src/main/java/junior/test/task/repository/TransactionRepository.java
+++ src/main/java/junior/test/task/repository/TransactionRepository.java
@@ -2,8 +2,17 @@ package junior.test.task.repository;
 
 import junior.test.task.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+  @Query("SELECT t FROM Transaction t WHERE t.goods_limit_exceeded = true")
+  List<Transaction> findAllByGoodsLimitExceededIsTrue();
+
+  @Query("SELECT t FROM Transaction t WHERE t.service_limit_exceeded = true")
+  List<Transaction> findAllByServiceLimitExceededIsTrue();
+
 }
--- src/main/java/junior/test/task/service/TransactionService.java
+++ src/main/java/junior/test/task/service/TransactionService.java
@@ -3,7 +3,6 @@ package junior.test.task.service;
 import junior.test.task.dto.TransactionDto;
 import junior.test.task.mapper.TransactionMapper;
 import junior.test.task.model.MonthlyLimit;
-import junior.test.task.model.Transaction;
 import junior.test.task.repository.MonthlyLimitRepository;
 import junior.test.task.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.util.List;
 
 
 @Service
@@ -22,9 +22,8 @@ public class TransactionService {
 
   public TransactionDto saveExpense(TransactionDto transactionDto) {
     transactionDto.setTransactionDate(LocalDate.now());
-    MonthlyLimit limitToday = new MonthlyLimit();
 
-    limitToday = monthlyLimitRepository.findAllByMonth(YearMonth.now());
+    MonthlyLimit limitToday = monthlyLimitRepository.findAllByMonth(YearMonth.now());
     if (transactionDto.getCategory().getId() == 1) {
       limitToday.setGoodsLimitUSD(limitToday.getGoodsLimitUSD() - transactionDto.getAmount());
     }else if (transactionDto.getCategory().getId() == 2) {
@@ -40,6 +39,17 @@ public class TransactionService {
 
       return transactionMapper.toDto(transactionRepository.save(transactionMapper.fromDto(transactionDto)));
   }
+
+  public List<TransactionDto> getGoodsExceededList(){
+    return transactionMapper.toTransactionDtoList(transactionRepository.
+            findAllByGoodsLimitExceededIsTrue());
+  }
+
+  public List<TransactionDto> getServicesExceededList(){
+    return transactionMapper.toTransactionDtoList(transactionRepository.
+            findAllByServiceLimitExceededIsTrue());
+  }
+
 }
 
 
